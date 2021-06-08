0da96c4777706c4474211ffa4556cd0c74cb4f7f
Phongvh <phongvh@haposoft.com>
2021-06-08 22:31:06 +0700

    graphQL with mockdata

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/hapo_ams_test', { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('connection to datbase')
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log('now listening for request on port 4000')
+})
